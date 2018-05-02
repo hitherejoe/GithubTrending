837e28356d4e67111d5f875564b6dfd92b4c6146
Joe Birch <hitherejoebirch@googlemail.com>
2018-05-02 20:12:29 +0100

    Fix presentation layer

--- Data/src/main/java/co/joebirch/data/mapper/ProjectMapper.kt
+++ Data/src/main/java/co/joebirch/data/mapper/ProjectMapper.kt
@@ -8,12 +8,12 @@ class ProjectMapper @Inject constructor(): EntityMapper<ProjectEntity, Project> 
 
     override fun mapFromEntity(entity: ProjectEntity): Project {
         return Project(entity.id, entity.name, entity.fullName, entity.starCount,
-                entity.dateCreated, entity.ownerName, entity.ownerAvatar)
+                entity.dateCreated, entity.ownerName, entity.ownerAvatar, entity.isBookmarked)
     }
 
     override fun mapToEntity(domain: Project): ProjectEntity {
         return ProjectEntity(domain.id, domain.name, domain.fullName, domain.starCount,
-                domain.dateCreated, domain.ownerName, domain.ownerAvatar, false)
+                domain.dateCreated, domain.ownerName, domain.ownerAvatar, domain.isBookmarked)
     }
 
 }
